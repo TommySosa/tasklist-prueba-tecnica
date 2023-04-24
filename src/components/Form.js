15aa795eeb545cdade553045976590ce7c3b8248
TommySosa <tomas.facundo.sosa@gmail.com>
2023-04-24 14:36:30 -0300

    Readme file

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+export function Form(props) {
+
+    const {task,handleSubmit, handleChange} = props;
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Introduce la tarea" 
+            onChange={handleChange} value={task}/>
+            <input type="submit" className="btn" value="AGREGAR" 
+            onClick={handleSubmit}/>
+        </form>
+    );
+
+}
